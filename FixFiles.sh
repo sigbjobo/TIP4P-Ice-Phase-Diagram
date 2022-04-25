89b9d04d539dc6080ec07ec8764d913533370d51
Sigbjørn Løland Bore <sigbjorn.loland.bore@gmail.com>
2022-04-25 16:20:03 -0700

    Updated plots to GPa

--- FixFiles.sh
+++ FixFiles.sh
@@ -4,5 +4,5 @@ find Ice*-Liquid*/*Biased*/* -name *.json | xargs -l bash -c ' sed -i "s/fig1_si
 find Ice*-Liquid*/*Biased*/* -name *.json | xargs -l bash -c ' sed -i "s/fig2_size.*/fig2_size\": [7,5],/g" $0'
 
 
-find IceII-Liquid/*Biased*/* -name *.json | xargs -l bash -c ' sed -i "s/fig1_size.*/fig1_size\": [12,3.5],/g" $0'
-find IceII-Liquid/*Biased*/* -name *.json | xargs -l bash -c ' sed -i "s/fig2_size.*/fig2_size\": [7,2.5],/g" $0'
+find IceII-Liquid/*Biased*/* -name *.json | xargs -l bash -c ' sed -i "s/fig1_size.*/fig1_size\": [12,6.5],/g" $0'
+find IceII-Liquid/*Biased*/* -name *.json | xargs -l bash -c ' sed -i "s/fig2_size.*/fig2_size\": [7,4.5],/g" $0'
