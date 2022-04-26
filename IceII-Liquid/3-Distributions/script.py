80681bc29cc60851dfe30d7023c6356ae8ff0ccc
Sigbjørn Løland Bore <sigbjorn.loland.bore@gmail.com>
2022-04-26 12:17:55 -0700

    removed distribution

--- IceII-Liquid/3-Distributions/script.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-import sys
-data1=np.genfromtxt("IceII_{}/histo".format(sys.argv[1]))
-data2=np.genfromtxt("Liquid_{}/histo".format(sys.argv[1]))
-print(np.trapz(np.minimum(data1[:,1],data2[:,1]),x=data1[:,0]))
-
--- IceV-Liquid/2-CalculateDistributions/script2.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-
-data1=np.genfromtxt("IceV/histo")
-data2=np.genfromtxt("Liquid/histo")
-print(np.trapz(np.minimum(data1[:,1],data2[:,1]),x=data1[:,0]))
-
